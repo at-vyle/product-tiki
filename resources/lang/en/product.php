044d955fc98a467fab9b1dda0c70df4effa3bd85
at-khadinh <kha.dinh@asiantech.vn>
2018-05-15 12:04:59 +0700

    init controller, index layout

---
+++ resources/lang/en/product.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'index' => [
+        'id' => 'Id',
+        'category_id' => 'Category Id',
+        'name' => 'Name',
+        'description' => 'Description',
+        'avg_rating' => 'Avg Rating',
+        'price' => 'Price',
+        'status' => 'Status',
+        'image' => 'Image',
+        'created_at' => 'Created At',
+        'update_at' => 'Update At',
+        'action' => 'Action',
+        'view' => 'View'
+    ]
+];
