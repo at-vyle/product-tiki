17979141261054fc0f70ab0bca9e9a18ca946848
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-05-15 10:28:29 +0700

    upload post-list layout

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -58,7 +58,7 @@ class PostController extends Controller
      */
     public function showComments()
     {
-        return 'abc';
+        //
     }
 
         /**
@@ -68,7 +68,7 @@ class PostController extends Controller
      */
     public function showReviews()
     {
-        return 'abc';
+        //
     }
 
     /**
