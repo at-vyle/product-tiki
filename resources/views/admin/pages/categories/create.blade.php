1411e40130f58c0e8488f595f25bac746de6502c
quangtrangle <trang.le2@asiantech.vn>
2018-05-16 10:30:27 +0700

    [Admin] layout create category

--- resources/views/admin/pages/categories/create.blade.php
+++ resources/views/admin/pages/categories/create.blade.php
@@ -33,7 +33,6 @@
               </div>
               <div class="form-group">
                 <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                  <button type="button" class="btn btn-primary">Cancel</button>
                   <button type="reset" class="btn btn-primary">Reset</button>
                   <button type="submit" class="btn btn-success">Submit</button>
                 </div>
