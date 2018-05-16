4d2264e6d3490e94537b908c006b2db19576e57e
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-05-16 15:34:46 +0700

    fix call position seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
     }
 }
