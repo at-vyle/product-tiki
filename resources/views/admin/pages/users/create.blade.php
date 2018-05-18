355201eb8447ff5fc1ed64d314b9dabc3b5a78ec
at-mailuong <mai.luong@asiantech.vn>
2018-05-18 10:56:46 +0700

    validate form create user

--- resources/views/admin/pages/users/create.blade.php
+++ resources/views/admin/pages/users/create.blade.php
@@ -9,10 +9,21 @@
           <h2>{{ __('user.index.formdesign') }}</h2>
           <div class="clearfix"></div>
         </div>
+        @if (count($errors))
+          <div class="form-group">
+              <div class="alert alert-error">
+                <ul>
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div>
+            </div>
+        @endif
         <div class="x_content">
           <br>
-          <form id="demo-form2" data-parsley-validate="" class="form-horizontal form-label-left" novalidate="">
-
+          <form id="demo-form2" method="POST" action="{{route('admin.users.store')}}" enctype="multipart/form-data" class="form-horizontal form-label-left">
+            @csrf
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="username">{{ __('user.index.username') }}<span class="required">*</span>
               </label>
@@ -24,44 +35,44 @@
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">{{ __('user.index.email') }}<span class="required">*</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="email" name="email"  class="form-control col-md-7 col-xs-12">
+                <input type="text" id="email" name="email" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password">{{ __('user.index.password') }}<span class="required">*</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="password" id="password" name="password"  class="form-control col-md-7 col-xs-12">
+                <input type="password" id="password" name="password" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fullname">{{ __('user.index.fullname') }}<span class="required">*</span></label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="fullname" name="fullname"  class="form-control col-md-7 col-xs-12">
+                <input type="text" id="fullname" name="fullname" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="address">{{ __('user.index.address') }}<span class="required">*</span></label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="address" name="address"  class="form-control col-md-7 col-xs-12">
+                <input type="text" id="address" name="address" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="phone">{{ __('user.index.phone') }}<span class="required">*</span></label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="phone" name="phone"  class="form-control col-md-7 col-xs-12">
+                <input type="text" id="phone" name="phone" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="indentity-card">{{ __('user.index.indentity_card') }}<span class="required">*</span></label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="indentity" name="indentity"  class="form-control col-md-7 col-xs-12">
+                <input type="text" id="indentity" name="indentity" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="avatar">{{ __('user.index.avatar') }}<span class="required">*</span></label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="file" id="avatar" name="avatar"  class="form-control col-md-7 col-xs-12">
+                <input type="file" id="avatar" name="avatar" class="form-control col-md-7 col-xs-12">
               </div>
             </div>
             <div class="form-group">
