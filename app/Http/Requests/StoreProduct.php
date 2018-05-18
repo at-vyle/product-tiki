acde19ee77bc49bf487b94f7e4addf72beb00f9c
at-khadinh <kha.dinh@asiantech.vn>
2018-05-18 18:16:44 +0700

    fix convention

--- app/Http/Requests/StoreProduct.php
+++ app/Http/Requests/StoreProduct.php
@@ -24,11 +24,11 @@ class StoreProduct extends FormRequest
     public function rules()
     {
         return [
-            'category_id' => 'required',
+            'category_id' => 'required|integer|min:0',
             'name' => 'required|string|max:255',
             'description' => 'string',
-            'price' => 'required|int|min:0',
-            'quantity' => 'required|int|min:0',
+            'price' => 'required|integer|min:0',
+            'quantity' => 'required|integer|min:0',
             'input_img' => 'required|image|mimes:jpeg,png,jpg,gif,svg'
         ];
     }
