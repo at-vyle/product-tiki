8929700048d27c91a6c6958d3ffa3a60a1a33276
at-khadinh <kha.dinh@asiantech.vn>
2018-05-22 23:10:25 +0700

    rename StoreProduct file, change validation

--- app/Http/Requests/PostProductRequest.php
+++ app/Http/Requests/PostProductRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreProduct extends FormRequest
+class PostProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class StoreProduct extends FormRequest
     public function rules()
     {
         return [
-            'category_id' => 'required|integer|min:0',
+            'category_id' => 'required|integer|min:1',
             'name' => 'required|string|max:255',
             'description' => 'string',
             'price' => 'required|integer|min:0',
