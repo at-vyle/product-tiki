a0007755aa867224ba45fe66d2872bf22ae161d4
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-05-23 09:07:13 +0700

    add feature register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -61,14 +61,16 @@ class RegisterController extends Controller
      *
      * @param array $data Data user
      *
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'old_password' => '',
+            'api_token' => ''
         ]);
     }
 }
