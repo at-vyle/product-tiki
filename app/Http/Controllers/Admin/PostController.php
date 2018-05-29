e30b75bee245ab8ed0eb85c69dd64bc655a628bd
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-05-29 15:22:13 +0700

    fix post-details and add feature sort posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -25,9 +25,25 @@ class PostController extends Controller
         })->when(isset($request->post_status), function ($query) use ($request) {
             return $query->where('status', '=', $request->post_status);
         })
-        ->with(['user','product'])->paginate($perPage);
-        $posts->appends(request()->query());
-        $data['posts'] = $posts;
+        ->with(['user' => function ($query) {
+            return $query->with('userInfo');
+        },'product']);
+        \DB::enableQueryLog();
+        if (isset($request->sortBy) && isset($request->dir)) {
+            $postSort = $posts->join('users', 'posts.user_id', 'users.id')
+                        ->join('products', 'posts.product_id', 'products.id')
+                        ->orderBy($request->sortBy, $request->dir)
+                        ->paginate($perPage);
+            $postSort->appends(request()->query());
+            // dd($postSort);
+            $data['posts'] = $postSort;
+        } else {
+            $posts = $posts->orderBy('id', 'desc')->paginate($perPage);
+            $posts->appends(request()->query());
+            $data['posts'] = $posts;
+        }
+        // dd(\DB::getQueryLog());
+
         return view('admin.pages.posts.index', $data);
     }
 
@@ -43,13 +59,16 @@ class PostController extends Controller
     public function show(Request $request, $id)
     {
         $perPage = config('define.post.limit_rows');
-        $comments = Post::find($id)->comments()->when(isset($request->content), function ($query) use ($request) {
+        $post = Post::with(['user' => function ($query) {
+            return $query->with('userInfo');
+        }, 'product'])->find($id);
+        $comments = $post->comments()->when(isset($request->content), function ($query) use ($request) {
             return $query->where('content', 'like', "%$request->content%");
         })
         ->with('user')->paginate($perPage);
         $comments->appends(request()->query());
+        $data['post'] = $post;
         $data['comments'] = $comments;
-        $data['post_id'] = $id;
         return view('admin.pages.posts.show', $data);
     }
 
--- resources/views/admin/pages/posts/index.blade.php
+++ resources/views/admin/pages/posts/index.blade.php
@@ -13,17 +13,17 @@
         <div class="title_right">
           <div class="col-md-10 col-sm-10 col-xs-12 form-group pull-right top_search">
             <form action="{{ route('admin.posts.index') }}" method="GET">
-              <div class="input-group">   
+              <div class="input-group">
                 <div class="col-md-6">
                   <select name="post_status" class="form-control">
                     <option value="">{{ __('post.admin.list.subtitle_index') }}</option>
                     <option value="{{ App\Models\Post::UNAPPROVED }}">{{ __('post.admin.list.unapproved_post') }}</option>
                     <option value="{{ App\Models\Post::APPROVED }}">{{ __('post.admin.list.approved_post') }}</option>
                   </select>
-                </div>          
+                </div>
                 <div class="col-md-6">
                   <input type="text" name="content" class="form-control" placeholder="{{ __('post.admin.list.search') }}">
-                </div>        
+                </div>
                 <span class="input-group-btn">
                   <button class="btn btn-default" type="submit">{{ __('post.admin.list.go') }}</button>
                 </span>
@@ -36,7 +36,7 @@
       <div class="clearfix"></div>
 
       <div class="row">
-      
+
         <div class="clearfix"></div>
 
         <div class="col-md-12 col-sm-12 col-xs-12">
@@ -48,15 +48,35 @@
               <div class="clearfix"></div>
             </div>
               <h2 id="info-message">@if (session()->has('message')) {{ session()->pull('message', 'default') }} @endif</h2>
-            
+
             <div class="x_content" class="list-table">
               <table class="table table-hover">
                 <thead>
                   <tr>
-                    <th class="col-md-1">{{ __('post.admin.list.user_col') }}</th>
-                    <th class="col-md-2">{{ __('post.admin.list.product_col') }}</th>
+                    <th class="col-md-2">{{ __('post.admin.list.avatar_col') }}</th>
+                    <th class="col-md-2">{{ __('post.admin.list.user_col') }}
+                      @if (app('request')->input('dir') == 'DESC' && app('request')->input('sortBy') == 'username')
+                        <a href="{{ route('admin.posts.index', ['sortBy' => 'username', 'dir' => 'ASC']) }}">
+                          <i class="fa fa-sort-up"></i>
+                        </a>
+                      @else
+                        <a href="{{ route('admin.posts.index', ['sortBy' => 'username', 'dir' => 'DESC']) }}">
+                          <i class="fa fa-sort-down"></i>
+                        </a>
+                      @endif
+                    </th>
+                    <th class="col-md-3">{{ __('post.admin.list.product_col') }}
+                      @if (app('request')->input('dir') == 'DESC' && app('request')->input('sortBy') == 'name')
+                        <a href="{{ route('admin.posts.index', ['sortBy' => 'name', 'dir' => 'ASC']) }}">
+                          <i class="fa fa-sort-up"></i>
+                        </a>
+                      @else
+                        <a href="{{ route('admin.posts.index', ['sortBy' => 'name', 'dir' => 'DESC']) }}">
+                          <i class="fa fa-sort-down"></i>
+                        </a>
+                      @endif
+                    </th>
                     <th class="col-md-1">{{ __('post.admin.list.type_col') }}</th>
-                    <th class="col-md-4">{{ __('post.admin.list.content_col') }}</th>
                     <th class="col-md-1">{{ __('post.admin.list.status_col') }}</th>
                     <th class="col-md-1"># <i class="fa fa-star"></i></th>
                     <th class="col-md-2">{{ __('post.admin.list.action_col') }}</th>
@@ -65,18 +85,18 @@
                 <tbody>
                   @foreach ($posts as $post )
                   <tr>
+                    <td><img style="width:100px;height:100px" src="{{ $post['user']['userInfo']['avatar_url'] }}" alt=""></td>
                     <td>{{ $post['user']->username }}</td>
                     <td>{{ $post['product']->name }}</td>
                     <td>
-                      @if ($post['type'] == App\Models\Post::TYPE_REVIEW ) 
+                      @if ($post['type'] == App\Models\Post::TYPE_REVIEW )
                         {{ __('post.admin.form.type_reviews') }}
-                      @else 
+                      @else
                         {{ __('post.admin.form.type_comments') }}
                       @endif
                     </td>
-                    <td>{{ $post['content'] }}</td>
                     <td id='status{{ $post['id'] }}'>
-                        @if ($post['status'] ) 
+                        @if ($post['status'] )
                           {{ __('common.approve') }}
                         @else
                           {{ __('common.pending') }}
@@ -112,7 +132,7 @@
           </div>
         </div>
         {{ $posts->render() }}
-        <div class="clearfix"></div>       
+        <div class="clearfix"></div>
       </div>
     </div>
   </div>
