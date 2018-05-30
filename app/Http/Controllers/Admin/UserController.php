d8564145947f4481af19acd664edf7d2918e3e1e
at-mailuong <mai.luong@asiantech.vn>
2018-05-30 13:33:21 +0700

    fix convention

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id id
+     * @param int $user user
      *
      * @return \Illuminate\Http\Response
      */
