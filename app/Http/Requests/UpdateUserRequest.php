ad59e7985dbce737a2a81b0290df5abeb0e3d308
at-mailuong <mai.luong@asiantech.vn>
2018-05-30 22:28:45 +0700

    fix update gender

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -27,14 +27,13 @@ class UpdateUserRequest extends FormRequest
     {
         $user = $this->route()->parameter('user');
         $userInfo = User::with('userInfo')->where('id', $user->id)->get();
-        $identity = $userInfo[0]['userInfo']->identity_card;
         return [
             'fullname'       => 'string|max:255',
             'avatar'         => 'image|mimes:png,jpg,jpeg',
             'birthday'       => 'date_format:"Y-m-d"',
             'address'        => 'string|max:255',
             'phone'          => 'regex:/\(?([0-9]{3})\)?([ . -]?)([0-9]{3})\2([0-9]{4})/',
-            'identity_card'  => 'regex:/\(?([0-9]{3})\)?([ . -]?)([0-9]{3})\2([0-9]{3})/|unique:user_info,identity_card,' . $identity .',identity_card',
+            'identity_card'  => 'regex:/\(?([0-9]{3})\)?([ . -]?)([0-9]{3})\2([0-9]{3})/|unique:user_info,identity_card,' . $userInfo[0]['userInfo']->identity_card .',identity_card',
         ];
     }
 }
--- resources/views/admin/pages/users/edit.blade.php
+++ resources/views/admin/pages/users/edit.blade.php
@@ -14,23 +14,16 @@
           <br>
           @if (count($errors))
             <div class="form-group">
-<<<<<<< HEAD
               <div class="alert alert-error">
                 <ul>
                   @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                   @endforeach
                 </ul>
-=======
-              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="username">{{ __('user.index.username') }}<span class="required">{{ __('user.index.requied') }}</span>
-              </label>
-              <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="username" name="username" readonly="readonly" value="" class="form-control col-md-7 col-xs-12" placeholder="{{ $user->username }}">
->>>>>>> 64b9d925966d3a643785824f18d10d5a583187a5
               </div>
             </div>
           @endif
-          <form id="demo-form2" method="POST" action="{!! route('admin.users.update', array('id' => $result->id)) !!}" enctype="multipart/form-data" class="form-horizontal form-label-left">
+          <form id="demo-form2" method="POST" action="{!! route('admin.users.update', array('id' => $user->id)) !!}" enctype="multipart/form-data" class="form-horizontal form-label-left">
             @csrf
             @method('PUT')
 
@@ -38,14 +31,14 @@
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="username">{{ __('user.index.username') }}<span class="required">{{ __('user.index.requied') }}</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="username" name="username" readonly="readonly" value="{{ $result->username }}" class="form-control col-md-7 col-xs-12" >
+                <input type="text" id="username" name="username" readonly="readonly" value="{{ $user->username }}" class="form-control col-md-7 col-xs-12" >
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">{{ __('user.index.email') }}<span class="required">{{ __('user.index.requied') }}</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" id="email" name="email" value="{{ $result->email }}" readonly="readonly" class="form-control col-md-7 col-xs-12" >
+                <input type="text" id="email" name="email" value="{{ $user->email }}" readonly="readonly" class="form-control col-md-7 col-xs-12" >
               </div>
             </div>
             <div class="form-group">
@@ -83,15 +76,15 @@
               <label class="control-label col-md-3 col-sm-3 col-xs-12">{{ __('user.index.gender') }}</label>
               <div class="col-md-6 col-sm-6 col-xs-12">
                 <div id="gender" class="btn-group" data-toggle="buttons">
-                
+                  @if ($user->userInfo['gender'] == 0)
                   <label class="btn btn-default @if (!$user->userInfo['gender']) active @endif" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                    <input type="radio" name="gender" value="{{ $user->userInfo['gender'] }}" data-parsley-multiple="gender">&nbsp; {{ __('user.index.male') }}&nbsp; 
+                    <input type="radio" name="gender" value="0" data-parsley-multiple="gender">&nbsp; {{ __('user.index.male') }}&nbsp; 
                   </label>
-               
+                  @else
                   <label class="btn btn-default @if ($user->userInfo['gender']) active @endif" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                    <input type="radio" name="gender" value="{{ $user->userInfo['gender'] }}" data-parsley-multiple="gender"> {{ __('user.index.female') }}
+                    <input type="radio" name="gender" value="1" data-parsley-multiple="gender"> {{ __('user.index.female') }}
                   </label>
-                
+                  @endif
                 </div>
               </div>
             </div>
