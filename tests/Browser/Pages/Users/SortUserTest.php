f318b0027e3b2c6f9b78781f499394cf4c887fdb
at-mailuong <mai.luong@asiantech.vn>
2018-06-04 10:38:45 +0700

    testing sort url

--- tests/Browser/Pages/Users/SortUserTest.php
+++ tests/Browser/Pages/Users/SortUserTest.php
@@ -6,6 +6,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\User;
+use App\Models\UserInfo;
 
 class SortUserTest extends DuskTestCase
 {
@@ -23,5 +24,24 @@ class SortUserTest extends DuskTestCase
         factory(User::class, 10)->create();
     }
 
-    
+    /*
+     * A Dusk test for click link sort
+     *
+     * @return void
+     */
+    public function testClickLinksSort()
+    { 
+        $sortUsers = ['id', 'fullname'];
+        $this->browse(function (Browser $browser) use ($sortUsers) {
+            $browser->visit('/admin/users');
+            foreach ($sortUsers as $sortUser) {
+                $browser->click("#sort-link-$sortUser")
+                    ->assertQueryStringMissing('sort', $sortUser)
+                    ->assertQueryStringMissing('order', 'asc')
+                    ->click("#sort-link-$sortUser")
+                    ->assertQueryStringMissing('sort', $sortUser)
+                    ->assertQueryStringMissing('order', 'desc');
+            }
+        });
+    }
 }
