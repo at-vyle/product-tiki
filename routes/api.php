86755d6df3d31dab58c90fdb8c8295c8c5b5e2eb
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-06-05 17:43:05 +0700

    init trait response

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.api.' , 'namespace' => 'Api'], function () {
+Route::group(['prefix' => 'admin', 'as' => 'admin.api.' , 'namespace' => 'Api\Admin'], function () {
     Route::put('posts/{id}/status', 'PostController@changeStatus')->name('posts.update.status');
     Route::apiResource('images', 'ImageController')->parameters(['images' => 'id']);
     Route::put('orders/{order}/status', 'OrderController@changeStatus')->name('orders.update.status');
     Route::apiResource('comments', 'CommentController')->parameters(['comments' => 'id']);
+});
+
+Route::group(['as' => 'api.', 'namespace' => 'Api\User'], function () {
+    Route::apiResource('products', 'ProductController');
 });
