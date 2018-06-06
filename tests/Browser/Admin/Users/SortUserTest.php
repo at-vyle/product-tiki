39966eb87b09a5fac5f1f1075022d8cb13a04881
at-mailuong <mai.luong@asiantech.vn>
2018-06-06 08:57:16 +0700

    unittest sort user

--- tests/Browser/Admin/Users/SortUserTest.php
+++ tests/Browser/Admin/Users/SortUserTest.php
@@ -21,8 +21,8 @@ class SortUserTest extends DuskTestCase
     public function setUp()
     {
         parent::setUp();
-        $userIds = factory(User::class, 5)->create();
-        for ($i = 0; $i < 5; $i++) {
+        factory(User::class, 10)->create();
+        for ($i = 0; $i < 10; $i++) {
             factory(UserInfo::class)->create([
                 'user_id' => $i+1,     
             ]);
@@ -64,30 +64,73 @@ class SortUserTest extends DuskTestCase
     }
 
     /**
-     * A Dusk test data sort list user by name
-     * 
+     * A Dusk test sort user by name
+     *
+     * @param string $name name of column
+     * @param string $order order in database
+     * @param string $column column index of column
+     *
      * @dataProvider dataForTest
      *
      * @return void
      */
-    public function testSortListUser($name, $order, $column)
+    public function testSortUser($name, $order, $column)
     {
         $this->browse(function (Browser $browser) use ($name, $order, $column) {
             $browser->visit('admin/users')
                 ->click("#sort-link-$name a");
 
-            //Test list user Asc
+            //Test user Asc
             $arrayAsc = User::join('user_info', 'users.id', 'user_info.user_id')->orderBy($order, 'asc')->pluck($order)->toArray();
             for ($i = 1; $i <= 5; $i++) {
                 $selector = "#table-user tbody tr:nth-child($i) td:nth-child($column)";
                 $this->assertEquals($browser->text($selector), $arrayAsc[$i - 1]);
             }
-            // Test list user Desc
+
+            // Test user Desc
             $browser->click("#sort-link-$name a");
             $arrayDesc = User::join('user_info', 'users.id', 'user_info.user_id')->orderBy($order, 'desc')->pluck($order)->toArray();
             for ($i = 1; $i <= 5; $i++) {
                 $selector = "#table-user tbody tr:nth-child($i) td:nth-child($column)";
                 $this->assertEquals($browser->text($selector), $arrayDesc[$i - 1]);
+            }
+        });
+    }
+
+    /**
+     * A Dusk test data when panigate.
+     *
+     * @param string $name name of column
+     * @param string $order order in database
+     * @param string $column column index of column
+     *
+     * @dataProvider dataForTest
+     *
+     * @return void
+     */
+    public function testSortUsersPanigate($name, $order, $column)
+    {
+        $this->browse(function (Browser $browser) use ($name, $order, $column) {
+            $browser->visit('admin/users')
+                ->click("#sort-link-$name a")
+                ->clickLink("2");
+
+            // Test list Asc
+            $arrayAsc = User::join('user_info', 'users.id', 'user_info.user_id')->orderBy($order, 'asc')->pluck($order)->toArray();
+            $arraySortAsc = array_chunk($arrayAsc, 5)[1];
+            for ($i = 1; $i <= 5; $i++) {
+                $selector = "#table-user tbody tr:nth-child($i) td:nth-child($column)";
+                $this->assertEquals($browser->text($selector), $arraySortAsc[$i - 1]);
+            }
+
+            // Test list Desc
+            $browser->click("#sort-link-$name a")
+                ->clickLink("2");
+            $arrayDesc = User::join('user_info', 'users.id', 'user_info.user_id')->orderBy($order, 'desc')->pluck($order)->toArray();
+            $arraySortDesc = array_chunk($arrayDesc, 5)[1];
+            for ($i = 1; $i <= 5; $i++) {
+                $selector = "#table-user tbody tr:nth-child($i) td:nth-child($column)";
+                $this->assertEquals($browser->text($selector), $arraySortDesc[$i - 1]);
             }
         });
     }
