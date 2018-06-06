926385333859ca11c37b354fe2c59bff7331abf7
at-khadinh <kha.dinh@asiantech.vn>
2018-06-06 16:10:12 +0700

    Api get categories

---
+++ app/Http/Controllers/Api/User/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
+use App\Models\Category;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends ApiController
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categoriesAll = Category::all();
+        $categories = Category::where('level', 0)->get();
+        $categories = $this->getCategoryChilds($categories, $categoriesAll);
+
+        return $this->showAll($categories, Response::HTTP_OK);
+    }
+
+    /**
+     * Get all childs of categories.
+     *
+     * @param array $categories    categories level 0
+     * @param array $categoriesAll all categories
+     *
+     * @return array  $categories
+     */
+    public function getCategoryChilds($categories, $categoriesAll)
+    {
+        foreach ($categories as $category) {
+            $childCategories = [];
+            foreach ($categoriesAll as $categoryCheck) {
+                if ($category->id == $categoryCheck->parent_id) {
+                    array_push($childCategories, $categoryCheck);
+                    $category['child_categories'] = $childCategories;
+                }
+            }
+            $this->getCategoryChilds($childCategories, $categoriesAll);
+        }
+        return $categories;
+    }
+}
