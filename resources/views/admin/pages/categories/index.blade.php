7b54ecb8f23f093674e7b08c7d013cd5c924630e
quangtrangle <trang.le2@asiantech.vn>
2018-06-07 11:34:46 +0700

    fix cmt review

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -1,8 +1,6 @@
 @extends('admin.layout.master')
 @section('title', __('category.admin.title') )
 @section('content')
-<script src="/js/deleteCategory.js"></script>
-<script src="/js/messages.js"></script>
 <div class="right_col" role="main">
   <div class="">
     <div class="row">
