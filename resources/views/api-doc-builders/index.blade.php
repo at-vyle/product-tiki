88125c2d022f7c28ab84c3f1b6bde323b420180b
quangtrangle <trang.le2@asiantech.vn>
2018-06-08 16:49:43 +0700

    write docs api get detail product

--- resources/views/api-doc-builders/index.blade.php
+++ resources/views/api-doc-builders/index.blade.php
@@ -1,3 +1,4 @@
 {!! file_get_contents(base_path('docs/api-references/api.md')) !!}
 {!! file_get_contents(base_path('docs/api-references/category.md')) !!}
 {!! file_get_contents(base_path('docs/api-references/product.md')) !!}
+{!! file_get_contents(base_path('docs/api-references/detail_product.md')) !!}
