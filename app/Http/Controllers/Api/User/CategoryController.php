b51729092c3742b0ae61317ef81c82fa514c3321
at-khadinh <kha.dinh@asiantech.vn>
2018-06-11 12:37:51 +0700

    show three level category

--- app/Http/Controllers/Api/User/CategoryController.php
+++ app/Http/Controllers/Api/User/CategoryController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends ApiController
 {
-
     /**
      * Display a listing of the resource.
      *
@@ -17,33 +16,8 @@ class CategoryController extends ApiController
      */
     public function index()
     {
-        $categoriesAll = Category::all();
-        $categories = Category::where('level', 0)->get();
-        $categories = $this->getCategoryChilds($categories, $categoriesAll);
+        $categories = Category::where('level', Category::BASELEVEL)->with('children.children')->get();
 
         return $this->showAll($categories, Response::HTTP_OK);
-    }
-
-    /**
-     * Get all childs of categories.
-     *
-     * @param array $categories    categories level 0
-     * @param array $categoriesAll all categories
-     *
-     * @return array  $categories
-     */
-    public function getCategoryChilds($categories, $categoriesAll)
-    {
-        foreach ($categories as $category) {
-            $childCategories = [];
-            foreach ($categoriesAll as $categoryCheck) {
-                if ($category->id == $categoryCheck->parent_id) {
-                    array_push($childCategories, $categoryCheck);
-                    $category['child_categories'] = $childCategories;
-                }
-            }
-            $this->getCategoryChilds($childCategories, $categoriesAll);
-        }
-        return $categories;
     }
 }
