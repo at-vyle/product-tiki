750b073dce05e8c42ad61af497844ee8ac979f14
at-khadinh <kha.dinh@asiantech.vn>
2018-06-12 09:07:58 +0700

    define api for get product

--- app/Http/Controllers/Api/User/ProductController.php
+++ app/Http/Controllers/Api/User/ProductController.php
@@ -5,19 +5,47 @@ namespace App\Http\Controllers\Api\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ApiController;
 use App\Models\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
+     * @param \Illuminate\Http\Request $request request content
+     *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = $this->formatPaginate(Product::paginate(5));
+        $perPage = isset($request->perpage) ? $request->perpage : config('define.product.limit_rows');
+        $request->order = isset($request->order) ? $request->order : config('define.dir_asc');
 
-        return $this->showAll($products, 200);
+        $products = Product::when(isset($request->category), function ($query) use ($request) {
+            return $query->where('category_id', $request->category);
+        })->with("category", "images");
+
+        if (isset($request->sortBy) &&  $request->sortBy != 'selling') {
+            $products = $products->orderBy($request->sortBy, $request->order);
+        }
+
+        if (isset($request->limit)) {
+            $products = $products->limit($request->limit);
+        }
+
+        $products = $products->get();
+
+        foreach ($products as $product) {
+            $product['quantity_sold'] = $product->orderDetails()->sum('quantity');
+        }
+        if (isset($request->sortBy) && $request->sortBy == 'selling') {
+            $products = $products->sortByDesc('quantity_sold')->values();
+        }
+
+        $products = $this->paginate($products, $perPage);
+        $products = $this->formatPaginate($products);
+
+        return $this->showAll($products, Response::HTTP_OK);
     }
 
     /**
