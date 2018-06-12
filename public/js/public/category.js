d374c1733c42c71fe944acfe151acee12ccb6203
at-mailuong <mai.luong@asiantech.vn>
2018-06-12 10:31:48 +0700

    fix comment and fix categoryController

--- public/js/public/category.js
+++ public/js/public/category.js
@@ -1,32 +1,29 @@
 $.ajax({
     url: "/api/categories",
     type: "get",
-    data: {
-    },
     success: function( result ) {
         let html = '';
-        
         result.result.forEach(category => {
-            if (category.child_categories) {
-                let childsHtml = '';
-                category.child_categories.forEach(childsCategory => {
-                    childsHtml += '<li><a href="household.html">'+ childsCategory.name +'</a></li>';
+            let childsHtml = '';
+            if (category.children) {
+                category.children.forEach(childsCategory => {
+                    childsHtml += '<li><a href="#">'+ childsCategory.name +'</a></li>';
                 });
-                html += '<li class="dropdown">\
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">' + category.name + '<b class="caret"></b></a>\
-                            <ul class="dropdown-menu multi-column columns-3">\
-                                <div class="row">\
-                                    <div class="multi-gd-img">\
-                                        <ul class="multi-column-dropdown">\
+            }
+            html += '<li class="dropdown">\
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">' + category.name + '<b class="caret"></b></a>\
+                        <ul class="dropdown-menu multi-column columns-3">\
+                            <div class="row">\
+                                <div class="multi-gd-img">\
+                                    <ul class="multi-column-dropdown">\
                                         <h6>' + category.name + '</h6>\
                                         '+ childsHtml +'\
-                                        </ul>\
-                                    </div>\
+                                    </ul>\
                                 </div>\
-                            </ul>\
-                        </li>';
-            }
+                            </div>\
+                        </ul>\
+                    </li>';
         });
-        document.getElementsByClassName('navbar-nav')[0].innerHTML += html;
+        $('.navbar-nav').append(html);
     }
  });
