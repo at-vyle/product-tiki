df7fad67f6f8ca7a7665cb097746e8ea01e0b1ab
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-06-15 10:37:18 +0700

    fix layout profile index

--- resources/views/public/pages/profile.blade.php
+++ resources/views/public/pages/profile.blade.php
@@ -1,5 +1,5 @@
 @extends('public.layout.master')
-@section('title', __('user/master.title.product'))
+@section('title', __('user/profile.title'))
 @section('css')
   <link href="/css/public/profile.css" rel="stylesheet">
 @endsection
@@ -9,7 +9,7 @@
         <div class="col-md-12">
           <div class="page-title">
             <div class="title_left">
-              <h3>User Profile</h3>
+              <h3>{{ __('user/profile.title') }}</h3>
             </div>
           </div>
           <div class="clearfix"></div>
@@ -17,7 +17,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
               <div class="x_panel col-md-12">
                 <div class="x_title col-md-12">
-                  <h2>User Report <small>Activity report</small></h2>
+                  <h2>{{ __('user/profile.subtitle') }}</h2>
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content col-md-12">
@@ -28,29 +28,26 @@
                         <img class="img-responsive avatar-view" src="/images/avatar/img.jpg" alt="Avatar" title="Change the avatar">
                       </div>
                     </div>
-                    <h3>Samuel Doe</h3>
-                    <i class="fa fa-venus"></i>  
-                    <i class="fa fa-mars"></i>  
+
                     <ul class="list-unstyled user_data">
-                      <li><i class="fa fa-map-marker user-profile-icon"></i> San Francisco, California, USA</li>
                       <li class="m-top-xs">
-                        <i class="fa fa-birthday-cake"></i>
-                        <a href="">1-1-2011</a>
+                        <i class="fa fa-user"></i>
+                        <a>Samuel Doe</a>
+                      </li>
+                      <li class="m-top-xs">
+                        <i class="fa fa-venus"></i>
+                        <i class="fa fa-mars"></i>
+                      </li>
+                      <li class="m-top-xs">
+                        <i class="fa fa-map-marker user-profile-icon"></i>
+                        <a> San Francisco, California, USA</a>
                       </li>
                       <li class="m-top-xs">
                         <i class="fa fa-envelope user-profile-icon"></i>
-                        <a href="">admin@test.co</a>
-                      </li>
-                      <li class="m-top-xs">
-                        <i class="fa fa-phone"></i>
-                        <a href="">0123456789</a>
-                      </li>
-                      <li class="m-top-xs">
-                        <i class="fa fa-birthday-cake"></i>
-                        <a href="">1-1-2011</a>
+                        <a>admin@test.co</a>
                       </li>
                     </ul>
-
+                    <br>
                     <a class="btn btn-success"><i class="fa fa-edit m-right-xs"></i>Edit Profile</a>
                     <br />
 
@@ -58,151 +55,103 @@
                   <div class="col-md-9 col-sm-9 col-xs-12">
                     <div class="row" role="tabpanel" data-example-id="togglable-tabs">
                       <ul id="myTab" class="nav nav-tabs-bar bar_tabs" role="tablist">
-                        <li role="presentation" class="col-md-3 active"><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Recent Activity</a>
+                        <li role="presentation" class="col-md-3"><a href="#user_profile" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">{{ __('user/profile.user_profile') }}</a>
                         </li>
-                        <li role="presentation" class="col-md-3"><a href="#tab_content2" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Projects Worked on</a>
+                        <li role="presentation" class="col-md-3"><a href="#recent_activity" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">{{ __('user/profile.recent_activity') }}</a>
                         </li>
-                        <li role="presentation" class="col-md-3"><a href="#tab_content3" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">Profile</a>
+                        <li role="presentation" class="col-md-3"><a href="#recent_order" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">{{ __('user/profile.recent_order') }}</a>
                         </li>
                       </ul>
                       <div id="myTabContent" class="tab-content">
-                        <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
-
-                          <!-- start recent activity -->
-                          <ul class="messages">
-                            <li>
-                              <img src="/images/avatar/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-info">24</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Desmond Davison</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                                  <a href="#"><i class="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="/images/avatar/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-error">21</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Brian Michaels</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1" aria-hidden="true" data-icon=""></span>
-                                  <a href="#" data-original-title="">Download</a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="/images/avatar/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-info">24</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Desmond Davison</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                                  <a href="#"><i class="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="/images/avatar/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-error">21</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Brian Michaels</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1" aria-hidden="true" data-icon=""></span>
-                                  <a href="#" data-original-title="">Download</a>
-                                </p>
-                              </div>
-                            </li>
-
-                          </ul>
-                          <!-- end recent activity -->
-
-                        </div>
-                        <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
+                        <div role="tabpanel" class="tab-pane fade" id="user_profile" aria-labelledby="profile-tab">
 
                           <!-- start user projects -->
-                          <table class="data table table-striped no-margin">
-                            <thead>
-                              <tr>
-                                <th>#</th>
-                                <th>Project Name</th>
-                                <th>Client Company</th>
-                                <th class="hidden-phone">Hours Spent</th>
-                                <th>Contribution</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>1</td>
-                                <td>New Company Takeover Review</td>
-                                <td>Deveint Inc</td>
-                                <td class="hidden-phone">18</td>
-                                <td class="vertical-align-mid">
-                                  <div class="progress">
-                                    <div class="progress-bar progress-bar-success" data-transitiongoal="35"></div>
-                                  </div>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>2</td>
-                                <td>New Partner Contracts Consultanci</td>
-                                <td>Deveint Inc</td>
-                                <td class="hidden-phone">13</td>
-                                <td class="vertical-align-mid">
-                                  <div class="progress">
-                                    <div class="progress-bar progress-bar-danger" data-transitiongoal="15"></div>
-                                  </div>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>3</td>
-                                <td>Partners and Inverstors report</td>
-                                <td>Deveint Inc</td>
-                                <td class="hidden-phone">30</td>
-                                <td class="vertical-align-mid">
-                                  <div class="progress">
-                                    <div class="progress-bar progress-bar-success" data-transitiongoal="45"></div>
-                                  </div>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>4</td>
-                                <td>New Company Takeover Review</td>
-                                <td>Deveint Inc</td>
-                                <td class="hidden-phone">28</td>
-                                <td class="vertical-align-mid">
-                                  <div class="progress">
-                                    <div class="progress-bar progress-bar-success" data-transitiongoal="75"></div>
-                                  </div>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
+                          <ul class="user-info">
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.avatar') }}</h4>
+                                <img id="avatar" src="/images/avatar/img.jpg" alt="">
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.username_col') }}</h4>
+                                <blockquote id="username" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.email_col') }}</h4>
+                                <blockquote id="email" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.fullname_col') }}</h4>
+                                <blockquote id="full_name" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.address_col') }}</h4>
+                                <blockquote id="address" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.gender_col') }}</h4>
+                                <blockquote id="gender" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.dob_col') }}</h4>
+                                <blockquote id="dob" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.phone_col') }}</h4>
+                                <blockquote id="phone" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                            <li>
+                              <div class="message_wrapper">
+                                <h4 class="heading">{{ __('user/profile.id_col') }}</h4>
+                                <blockquote id="identity_card" class="message">Data</blockquote>
+                                <br />
+                              </div>
+                            </li>
+                          </ul>
                           <!-- end user projects -->
 
                         </div>
-                        <div role="tabpanel" class="tab-pane fade" id="tab_content3" aria-labelledby="profile-tab">
+                        <div role="tabpanel" class="tab-pane fade" id="recent_activity" aria-labelledby="profile-tab">
+
+                          <ul class="messages">
+                            <li>
+                              <div class="message_date">
+                                <h3 class="date text-info">24</h3>
+                                <p class="month">May</p>
+                              </div>
+                              <div class="message_wrapper">
+                                <h3 class="heading">{{ __('user/profile.product_name') }} </h3>
+                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
+                                <h5 class="date-info">5 Days ago</h5>
+                                <br />
+                              </div>
+                            </li>
+                          </ul>
+
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="recent_order" aria-labelledby="profile-tab">
                           <!-- start user projects -->
                           <table class="data table table-striped no-margin">
                             <thead>
