c8445db8520ab73fd4335fa659bea9e2d04187f8
quangtrangle <trang.le2@asiantech.vn>
2018-06-19 13:52:44 +0700

    fix cmt review

--- public/js/public/main.js
+++ public/js/public/main.js
@@ -1,26 +1,8 @@
-$.ajax({
-    url: '/api/products?sortBy=quantity_sold&perpage=9',
-    type: 'get',
-    success: function(response) {
-        let html = '';
-        response.result.data.forEach(product => {
-            let stars = '';
-            let productsUri = '/products/';
-            let id = product.id;
-            let imagePath = product.image_path;
-            let image = product.images[0]['img_url'];
-            let name = product.name;
-            let priceFormated = product.price_formated;
-            let rate = Math.round(product.avg_rating);
-            for (i=1; i<= 5; i++) {
-                if (i <= rate) {
-                    stars += '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
-                }
-                else {
-                    stars += '<i class="fa fa-star black-star" aria-hidden="true"></i>'
-                }
-            }
-            html+='<div class="col-md-4 top_brand_left">'+
+let productsUri = '/products/';
+let maxStar = 5;
+function generateProducts(imagePath, image, name, stars, priceFormated, productsUri, id) {
+    let htmlProduct = '';
+    htmlProduct +=  '<div class="col-md-4 top_brand_left">'+
                         '<div class="hover14 column">'+
                             '<div class="agile_top_brand_left_grid">'+
                                 '<div class="agile_top_brand_left_grid_pos">'+
@@ -45,25 +27,21 @@ $.ajax({
                             '</div>'+
                         '</div>'+
                     '</div>';
-        });
-        $('#expeditions .agile_top_brands_grids').append(html);
-    }
-});
+    return htmlProduct;
+}
 $.ajax({
-    url: '/api/products?sortBy=avg_rating&perpage=9',
+    url: '/api/products?sortBy=quantity_sold&perpage=9',
     type: 'get',
     success: function(response) {
-        let html = '';
         response.result.data.forEach(product => {
             let stars = '';
-            let productsUri = '/products/';
             let id = product.id;
             let imagePath = product.image_path;
             let image = product.images[0]['img_url'];
             let name = product.name;
             let priceFormated = product.price_formated;
             let rate = Math.round(product.avg_rating);
-            for (i=1; i<= 5; i++) {
+            for (i=1; i<= maxStar; i++) {
                 if (i <= rate) {
                     stars += '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
                 }
@@ -71,33 +49,34 @@ $.ajax({
                     stars += '<i class="fa fa-star black-star" aria-hidden="true"></i>'
                 }
             }
-            html+='<div class="col-md-4 top_brand_left">'+
-                        '<div class="hover14 column">'+
-                            '<div class="agile_top_brand_left_grid">'+
-                                '<div class="agile_top_brand_left_grid_pos">'+
-                                '</div>'+
-                                '<div class="agile_top_brand_left_grid1">'+
-                                '<figure>'+
-                                    '<div class="snipcart-item block" >'+
-                                    '<div class="snipcart-thumb">'+
-                                        '<a href="#"><img title=" " alt=" " src="'+ imagePath + image + '" /></a>'+  
-                                        '<p>'+ name +'</p>'+
-                                        '<div class="stars">' + stars + '</div>'+
-                                        '<h4>$'+ priceFormated +'<span></span></h4>'+
-                                    '</div>'+
-                                    '<div class="snipcart-details top_brand_home_details">'+
-                                        '<a href="' + productsUri + id + '">'+
-                                            '<input type="submit" name="submit" value="' + Lang.get('user/index.detail') + '" class="button" />'+
-                                        '</a>'+
-                                    '</div>'+
-                                    '</div>'+
-                                '</figure>'+
-                                '</div>'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>';
+            htmlProduct = generateProducts(imagePath, image, name, stars, priceFormated, productsUri, id);
+            $('#expeditions .agile_top_brands_grids').append(htmlProduct);
         });
-        $('#tours .agile_top_brands_grids').append(html);
+    }
+});
+$.ajax({
+    url: '/api/products?sortBy=avg_rating&perpage=9',
+    type: 'get',
+    success: function(response) {
+        response.result.data.forEach(product => {
+            let stars = '';
+            let id = product.id;
+            let imagePath = product.image_path;
+            let image = product.images[0]['img_url'];
+            let name = product.name;
+            let priceFormated = product.price_formated;
+            let rate = Math.round(product.avg_rating);
+            for (i=1; i<= maxStar; i++) {
+                if (i <= rate) {
+                    stars += '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
+                }
+                else {
+                    stars += '<i class="fa fa-star black-star" aria-hidden="true"></i>'
+                }
+            }
+            html = generateProducts(imagePath, image, name, stars, priceFormated, productsUri, id);
+            $('#tours .agile_top_brands_grids').append(html);
+        });
     }
 });
 $.ajax({
@@ -107,14 +86,13 @@ $.ajax({
         let html = '';
         response.result.data.forEach(product => {
             let stars = '';
-            let productsUri = '/products/';
             let id = product.id;
             let imagePath = product.image_path;
             let image = product.images[0]['img_url'];
             let name = product.name;
             let priceFormated = product.price_formated;
             let rate = Math.round(product.avg_rating);
-            for (i=1; i<= 5; i++) {
+            for (i=1; i<= maxStar; i++) {
                 if (i <= rate) {
                     stars += '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
                 }
@@ -122,7 +100,7 @@ $.ajax({
                     stars += '<i class="fa fa-star black-star" aria-hidden="true"></i>'
                 }
             }
-            html+='<div class="col-md-3 top_brand_left">'+
+            html += '<div class="col-md-3 top_brand_left">'+
                         '<div class="hover14 column">'+
                             '<div class="agile_top_brand_left_grid">'+
                                 '<div class="agile_top_brand_left_grid_pos">'+
