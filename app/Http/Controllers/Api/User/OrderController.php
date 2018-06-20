5207dd70ef7a459fb77cfad7c6577dc7fe85862b
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-06-20 14:40:06 +0700

    init api create order

--- app/Http/Controllers/Api/User/OrderController.php
+++ app/Http/Controllers/Api/User/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ApiController;
 use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Response;
 use Auth;
 
@@ -22,5 +23,34 @@ class OrderController extends ApiController
         $orders = Order::with('user')->withCount('orderDetails')->where('user_id', $user->id)->paginate($perPage);
         $data = $this->formatPaginate($orders);
         return $this->showAll($data, Response::HTTP_OK);
+    }
+
+    /**
+    * Create order
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function store()
+    {
+        $user = Auth::user();
+
+        $order = Order::create([
+            'user_id' => $user->id
+        ]);
+        $total = 0;
+        foreach (request('product') as $product) {
+            OrderDetail::create([
+                'product_id' => $product['id'],
+                'order_id' => $order->id,
+                'quantity' => $product['quantity'],
+                'product_price' => $product['price']
+            ]);
+            $total = $product['price'] * $product['quantity'];
+        }
+
+        $order->fill(['total' => $total]);
+        $order->load('orderDetails');
+
+        return $this->showOne($order, Response::HTTP_OK);
     }
 }
