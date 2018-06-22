cfc9dc57c423f72fbf6bc99b02eddbb3d9428722
Vũ Nam Phương <phuong.vu@asiantech.vn>
2018-06-22 10:36:28 +0700

    use TestCase setUp for test

--- tests/Feature/Api/CreatePostsTest.php
+++ tests/Feature/Api/CreatePostsTest.php
@@ -22,11 +22,6 @@ class CreatePostsTest extends TestCase
         parent::setUp();
         factory('App\Models\Category')->create();
         factory('App\Models\Product')->create();
-        $user = factory('App\Models\User')->create();
-        factory('App\Models\UserInfo')->create([
-            'user_id' => $user->id
-        ]);
-        Artisan::call('passport:install');
     }
 
     /**
@@ -73,26 +68,17 @@ class CreatePostsTest extends TestCase
      */
     public function testCreatePosts()
     {
-        $user = User::find(1);
-
-        $login = [
-            'email' => $user->email,
-            'password' => '12345'
-        ];
-
-        $response = $this->json('POST', '/api/login', $login, ['Accept' => 'application/json']);
-        $token = json_decode($response->getContent())->result->token;
         $posts = [
             'type' => 1,
             'content' => 'testing Content',
             'rating' => 3
         ];
 
-        $this->json('POST', 'api/products/1/posts', $posts, ['Accept' => 'application/json', 'Authorization' => 'Bearer '.$token])
+        $this->jsonUser('POST', 'api/products/1/posts', $posts)
             ->assertStatus(200)
             ->assertJsonStructure($this->jsonStructureCreatePostsSuccess());
 
-        $this->json('POST', 'api/products/1/posts', [], ['Accept' => 'application/json', 'Authorization' => 'Bearer '.$token])
+        $this->jsonUser('POST', 'api/products/1/posts')
             ->assertStatus(422)
             ->assertJsonStructure($this->jsonStructureCreatePostsFailValidation());
     }
