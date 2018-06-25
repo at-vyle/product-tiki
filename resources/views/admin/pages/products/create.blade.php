b5ab8cc59565fd532c49474663c892010d6a352e
at-khadinh <kha.dinh@asiantech.vn>
2018-06-25 09:39:46 +0700

    fix layout edit, create products

--- resources/views/admin/pages/products/create.blade.php
+++ resources/views/admin/pages/products/create.blade.php
@@ -77,7 +77,7 @@
             <div class="ln_solid"></div>
             <div class="form-group">
               <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                <button class="btn btn-primary" type="button">@lang('common.cancel-btn')</button>
+                <a href="{{ route('admin.products.index') }}" class="btn btn-primary">@lang('common.cancel-btn')</a>
                 <button type="submit" class="btn btn-success">@lang('product.create.create')</button>
               </div>
             </div>
