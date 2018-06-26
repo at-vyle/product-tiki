dcc19bb4bda0baa58e21129b52860a4147e751d0
at-mailuong <mai.luong@asiantech.vn>
2018-06-26 16:48:15 +0700

    unittest order detail

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        // $this->call(CategoriesTableSeeder::class);
-        // $this->call(ProductsTableSeeder::class);
-        $this->call(DemoTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
         $this->call(PostsTableSeeder::class);
         $this->call(UserInfoTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
