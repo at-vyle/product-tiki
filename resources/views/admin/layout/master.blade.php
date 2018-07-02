e95434272429074e3763cd93fd8655ba24340e8e
quangtrangle <trang.le2@asiantech.vn>
2018-07-02 10:42:27 +0700

    add ckeditor create and edit product page

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -259,7 +259,7 @@
     <script src="/js/admin/deleteAvatar.js"></script>
     <script src="/js/messages.js"></script>
     <script src="/js/admin/main.js"></script>
-    <script src="/ckeditor/ckeditor.js"></script>
+    <script src="bower_components/ckeditor/ckeditor.js"></script>
     @yield('js')
   </body>
 </html>
