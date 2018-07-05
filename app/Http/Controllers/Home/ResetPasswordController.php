66f3aca531a212b3663fe7e3d8d0e18d0905f4ba
at-khadinh <kha.dinh@asiantech.vn>
2018-07-05 15:12:42 +0700

    api reset password

--- app/Http/Controllers/Home/ResetPasswordController.php
+++ app/Http/Controllers/Home/ResetPasswordController.php
@@ -12,8 +12,9 @@ class ResetPasswordController extends Controller
      *
      * If no token is present, display the link request form.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string|null  $token
+     * @param \Illuminate\Http\Request $request request
+     * @param string|null              $token   reset token
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function showResetForm(Request $request, $token = null)
